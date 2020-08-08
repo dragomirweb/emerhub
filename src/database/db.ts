1729dc6fa5e33f15d69deddcf605ba902681aa16
Dragomir Dan <dan@dragomirweb.com>
2020-08-08 16:38:17 +0300

    add database config

---
+++ src/database/db.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Get a Firestore instance
+const app = firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY || '',
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "619001418589",
+  appId: "1:619001418589:web:9d998e954b1dd1de670eb4"
+})
+
+export const auth = firebase.auth(app)
+
+export const firestore = app.firestore()
+
